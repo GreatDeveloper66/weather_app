5e9201ff96a2c5f1e7be5d400eba3e851b723f3f
GreatDeveloper66 <ashaffe@gmail.com>
2024-05-27 21:09:52 -0500

    Add WeatherCard component

---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <div class="w3-card-4 w3-margin w3-white">
+        <img
+            src="https://www.w3schools.com/w3images/lights.jpg"
+            alt="Nature"
+            style="width:100%"
+        />
+        <div class="w3-container">
+            <h3>
+                <b>Paris</b>
+            </h3>
+            <p>Paris is the capital of France.</p>
+        </div>
+</template>
